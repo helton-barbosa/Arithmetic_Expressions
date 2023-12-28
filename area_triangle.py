e19329206e659a894f7948daa93cc70e289c070b
Helton Barbosa <heltonbs@gmail.com>
2023-12-28 19:50:13 -0300

    Cálculo da área do triângulo

---
+++ area_triangle.py
@@ -0,0 +1,4 @@
+base = int(input('Entre com o valor da base do triângulo: '))
+altura = int(input('Entre com o valor da altura do triângulo: '))
+area = (base * altura)/2
+print(f'A área do triângulo com base {base} e altura {altura} é: {area}')
