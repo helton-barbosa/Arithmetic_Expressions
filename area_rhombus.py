fa924b484bfc59721a16c7bd0cae28d0fa1d5886
Helton Barbosa <heltonbs@gmail.com>
2023-12-28 20:06:54 -0300

    Cálculo da área do losango

---
+++ area_rhombus.py
@@ -0,0 +1,5 @@
+diagonal_maior = int(input('Entre com o valor da diagonal maior do losango: '))
+diagonal_menor = int(input('Entre com o valor da diagonal menor do losango: '))
+area = (diagonal_maior * diagonal_menor) / 2
+print(f'A área do losango com diagonal maior de {diagonal_maior} e '
+      f'diagonal menor de {diagonal_menor} é: {area}')
